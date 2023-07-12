e68bb722ac5a2e17746ce4ca88aa619872116f61
alexxu06 <138343590+alexxu06@users.noreply.github.com>
2023-07-12 14:31:30 -0600

    Add files via upload

--- Website/script.js
+++ Website/script.js
@@ -1,20 +1,46 @@
-let aboutNav = document.getElementById("aboutNav")
-let teachingNav = document.getElementById("teachingNav")
-let publicationNav = document.getElementById("publicationNav")
+const aboutNav = document.getElementById("aboutNav")
+const teachingNav = document.getElementById("teachingNav")
+const researchNav = document.getElementById("researchNav")
+const publicationNav = document.getElementById("publicationNav")
 
-let aboutSection = document.getElementById("about")
-let teachingSection = document.getElementById("Teaching")
-let publicationsSection = document.getElementById("publications")
+const aboutSection = document.getElementById("about")
+const teachingSection = document.getElementById("Teaching")
+const researchSection = document.getElementById("Research")
+const publicationsSection = document.getElementById("publications")
 
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navigationLinks = document.getElementsByClassName("navigation")[0]
+
+let isOpen = false
 
 aboutNav.addEventListener("click", () => {
-    aboutSection.scrollIntoView({behavior: 'smooth'})
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
 })
 
 teachingNav.addEventListener("click", () => {
     teachingSection.scrollIntoView({behavior: 'smooth'})
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
+})
+
+researchNav.addEventListener("click", () => {
+    researchSection.scrollIntoView({behavior: 'smooth'})
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
 })
 
 publicationNav.addEventListener("click", () => {
     publicationsSection.scrollIntoView({behavior: 'smooth'})
+    if (isOpen) {
+        isOpen = navigationLinks.classList.toggle('active')
+    }
+})
+
+toggleButton.addEventListener('click', () => {
+    isOpen = navigationLinks.classList.toggle('active') 
 })
